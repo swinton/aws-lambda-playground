83a2104c8a1264c54001eb7809103b5918f2ac13
John Bohannon <imjohnbo@github.com>
2020-03-17 16:03:03 -0400

    feat: authenticate with app credentials instead of PAT

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 /* eslint-disable no-console */
 const { Octokit } = require('@octokit/rest');
+const { createAppAuth } = require('@octokit/auth-app');
 const decrypt = require('./lib/decrypt');
 
 exports.handler = async (event, context) => {
   console.log(event, context);
 
   // Decrypt token
-  const token = await decrypt(process.env.ENCRYPTED_GITHUB_TOKEN, { trim: true });
+  const privateKey = await decrypt(process.env.ENCRYPTED_PRIVATE_KEY, { trim: true });
+  const id = await decrypt(process.env.ENCRYPTED_APP_ID, { trim: true });
+
   const octokit = new Octokit({
-    auth: token
+    authStrategy: createAppAuth,
+    auth: {
+      id,
+      privateKey
+    }
   });
 
-  // Return the authenticated user
-  const { data: viewer } = await octokit.users.getAuthenticated();
+  // Return the authenticated app
+  const { data: viewer } = await octokit.apps.getAuthenticated();
   return viewer;
 };
 
