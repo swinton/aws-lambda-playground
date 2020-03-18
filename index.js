c763e6139c1ffe51f3c9317e9e51a898ed99f1d3
John Bohannon <imjohnbo@github.com>
2020-03-18 09:27:22 -0400

    chore: fix ESLint errors and remove decrypt

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 /* eslint-disable no-console */
 const { Octokit } = require('@octokit/rest');
 const { createAppAuth } = require('@octokit/auth-app');
-const decrypt = require('./lib/decrypt');
 
 exports.handler = async (event, context) => {
   console.log(event, context);
 
-  // Decode base64-encoded private key
-  const privateKey = Buffer.from(process.env.GITHUB_APP_PRIVATE_KEY, 'base64').toString()
-  const id = process.env.GITHUB_APP_ID
+  // Get GitHub App credentials from environment
+  const privateKey = Buffer.from(process.env.GITHUB_APP_PRIVATE_KEY, 'base64').toString();
+  const id = process.env.GITHUB_APP_ID;
 
+  // Create new octokit instance that is authenticated as an installation
   const octokit = new Octokit({
     authStrategy: createAppAuth,
     auth: {
